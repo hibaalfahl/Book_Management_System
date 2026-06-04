0de2e0b926c068ee3333c45d7cec1a1911422a8e
hibaalfahl <hibaalfahl25@gmail.com>
2026-06-04 12:37:59 +0300

    book management system

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index(){
+        $books = Book::query()->get();
+        return $books;
+    }
+
+    public function store(StoreBookRequest $request){
+        $book = Book::query()->create($request->validated());
+        return $book;
+    }
+    public function update(UpdateBookRequest $request,Book $book){
+       $book = $book->update($request->validated());
+        return $book;
+    }
+    public function show(Book $book){
+        return $book;
+    }
+    public function destroy(Book $book){
+        $book -> delete();
+    }
+}
---
+++ app/Http/Requests/UpdateBookRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+             'title'=>['nullable','string'],
+            'author'=>['nullable','string'],
+            'published_year'=>['nullable','integer','min:1900','max:2100'],
+            'is_available'=>['boolean']
+        ];
+    }
+}
